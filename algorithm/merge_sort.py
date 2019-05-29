4addc1162b3d70c8e3dfad8c078078fb74f4dfa3
gengxin <guogx@broada.com>
2019-05-29 16:46:10 +0800

    zengjia si

--- algorithm/merge_sort.py
+++ algorithm/merge_sort.py
@@ -1,6 +1,15 @@
 # coding=utf-8
 
 seq = [12, 23, 1, 23, 34, 4]
+def qsort(seq):
+    if seq == []:
+        return []
+    else:
+        pivot = seq[0]
+        less = qsort([x for x in seq[1:] if x < pivot])
+        greater = qsort([x for x in seq[1:] if x >= pivot])
+    return less + [pivot] + greater
+
 
 
 def merge(left, right):
@@ -33,4 +42,5 @@ def merge_sort(seq):
 
 
 if __name__ == '__main__':
+    print(qsort(seq))
     print (merge_sort(seq))
